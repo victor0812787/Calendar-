51bfa9322e14171284017467820b99662a17076a
victor0812787 <victoragbaoso15@gmail.com>
2022-11-08 01:31:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppHeader/>
+  <div class="w-full flex">
+    <!-- <DcHeros/> -->
+    <MyCalendar/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+import MyCalendar from './components/MyCalender.vue';
+// import DcHeros from './components/DcHeros';
+
+
+export default {
+      components:{AppHeader, MyCalendar},
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+   <nav class="w-full bg-gradient-to-r from-blue-800 to-pink-700 text-white px-4 py-2">
+      <a>MY CALENDER</a>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
